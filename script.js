ff465ececae8bcb96506a213b1c610799d9b8ab8
nb72 <nrddlnrdd@gmail.com>
2023-07-23 20:37:33 -0400

    third

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener('mousemove', function (e) {
+    let circle = document.getElementById('circle');
+    let left = e.pageX;
+    let top = e.pageY;
+    circle.style.left = left + 'px';
+    circle.style.top = top + 'px';
+});
+
+
+
