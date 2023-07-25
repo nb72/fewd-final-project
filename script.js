524afdb314729d62879605eba2570ea67c6e341f
nb72 <nrddlnrdd@gmail.com>
2023-07-25 01:57:48 -0400

    sixth

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //the cursor is following the mouse here:
-document.addEventListener('mousemove', function (e) {
-    preventdefault();
-    let circle = document.getElementById('circle');
-    let left = e.pageX;
-    let top = e.pageY;
-    circle.style.left = left + 'px';
-    circle.style.top = top + 'px';
-});
+// document.addEventListener('mousemove', function (e) {
+//     preventdefault();
+//     let circle = document.getElementById('circle');
+//     let left = e.pageX;
+//     let top = e.pageY;
+//     circle.style.left = left + 'px';
+//     circle.style.top = top + 'px';
+// });
 
 //the function that makes the words appear one by one:
 function typing(element, text, speed) {
@@ -26,13 +26,20 @@ document.addEventListener("DOMContentLoaded", function () {
     // preventdefault();
     const h2Element = document.getElementById("typewriter");
     const textToType = h2Element.textContent;
-    const typingSpeed = 100;
+    const typingSpeed = 60;
 
     h2Element.textContent = "";
     h2Element.style.visibility = "visible";
 
     typing(h2Element, textToType, typingSpeed);
 
+});
+
+$(document).mousemove(function (e) {
+    $("#head").css({
+        left: e.pageX,
+        top: e.pageY
+    });
 });
 
 
