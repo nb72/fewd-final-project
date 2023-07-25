dfda1e84791727c747fa36fafbe5259c63d8a64c
nb72 <nrddlnrdd@gmail.com>
2023-07-25 01:43:38 -0400

    fifth

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//the cursor is following the mouse here:
 document.addEventListener('mousemove', function (e) {
+    preventdefault();
     let circle = document.getElementById('circle');
     let left = e.pageX;
     let top = e.pageY;
@@ -6,16 +8,9 @@ document.addEventListener('mousemove', function (e) {
     circle.style.top = top + 'px';
 });
 
-document.addEventListener('mousemove', function (e) {
-    let circle = document.getElementById('circle');
-    let left = e.pageX;
-    let top = e.pageY;
-    circle.style.left = left + 'px';
-    circle.style.top = top + 'px';
-});
-
-
+//the function that makes the words appear one by one:
 function typing(element, text, speed) {
+    //preventdefault();
     let i = 0;
     const interval = setInterval(() => {
         element.textContent += text.charAt(i);
@@ -26,8 +21,9 @@ function typing(element, text, speed) {
     }, speed);
 }
 
-
+//the text that is being typed - only the first part:
 document.addEventListener("DOMContentLoaded", function () {
+    // preventdefault();
     const h2Element = document.getElementById("typewriter");
     const textToType = h2Element.textContent;
     const typingSpeed = 100;
@@ -36,39 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     h2Element.style.visibility = "visible";
 
     typing(h2Element, textToType, typingSpeed);
+
 });
 
 
-
-
-//need to connect the fucntion with mouse active on area here
-// document.addEventListener("DOMContentLoaded", function myFunction() {
-//     const h2Element = document.getElementById("typewriter1");
-//     const textToType = h2Element.textContent;
-//     const typingSpeed = 100;
-
-//     h2Element.textContent = "";
-//     h2Element.style.visibility = "visible";
-
-//     typing(h2Element, textToType, typingSpeed);
-// });
-
-// const options = {
-//     root: null,
-//     rootMargin: "0px",
-//     threshold: 0.5,
-// }
-
-
-// const observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("typewriter1");
-//             observer.unobserve(entry.target);
-//         }
-//     });
-// }, options);
-
-
-// const targetElement = document.getElementById("typewriter1");
-// observer.observe(targetElement);
